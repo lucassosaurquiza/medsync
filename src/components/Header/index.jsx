9edf28cc6393d7d5729e2198b8346add8441bf25
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2026-05-19 12:30:06 -0300

    fix/refactor: adding structures, methodology and components

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css'
+
+function Header () {
+  return (
+    <>
+      <header className='reserve-turn'>
+        <div className='reserve-turn__container'>
+          <h1 className='reserve-turn__title'>MedSync</h1>
+          <h2 className='reserve-turn__subtitle'>Reservar turno</h2>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export { Header }
---
+++ src/pages/ReserveYourTurn/index.jsx
@@ -0,0 +1,13 @@
+import { Header } from '../../components/Header'
+
+function ReserveYourTurn () {
+  return (
+    <>
+      <section>
+        <Header />
+      </section>
+    </>
+  )
+}
+
+export { ReserveYourTurn }
