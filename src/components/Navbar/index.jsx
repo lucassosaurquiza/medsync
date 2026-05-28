a2c038de42ee0478a97ee68254c006b0651236db
Martin2197 <martin.villalba2197@gmail.com>
2026-05-28 17:57:53 -0300

    fix/NavBarDasboard

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,27 @@
-function NavBar () {
+import { IoNotificationsOutline } from "react-icons/io5";
+import "./styles.css";
+
+
+function NavBar() {
   return (
-    <section>
-      <nav>
-        <h1>Esto es un Navbar, si tienes imaginacion</h1>
+    <section className="navbar-container">
+      <nav className="navbar">
+        <h1 className="reserve-turn__title">MedSync</h1> {/*Cambia el ClassName para utilizar los mismos estilos del resto.*/}
+
+        <div className="navbar-actions">
+          <button className="notification-btn">
+            <IoNotificationsOutline />
+          </button>
+
+          <img
+            className="profile-image"
+            src="https://i.pravatar.cc/150?img=12"
+            alt="profile"
+          />
+        </div>
       </nav>
     </section>
-  )
+  );
 }
 
 export { NavBar };
