69b3bdad4a97f176c340c7ac26ccba72b5a86d30
Martin2197 <martin.villalba2197@gmail.com>
2026-06-04 20:52:36 -0300

    Pequeños cambios

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,7 +6,8 @@ function Footer () {
       <h2 className='footer__title'>MedSync</h2>
       <p className='footer__info'>© 2024 MedSync Argentina. Tecnología para la salud.</p>
       <p className='footer__paragraph'>Creado por 
-        <a className='footer__obxel' href="https://obxel.vercel.app/" target="_blank" rel="noopener noreferrer">Obxel</a>
+        <a className='footer__obxel' href="https://obxel.vercel.app/" target="_blank" rel="noopener noreferrer">Obxel -</a>
+        <a className='footer__obxel' href="https://martin2197.github.io/Martin-portfolio/" target="_blank" rel="noopener noreferrer">Martin Villalba</a>
       </p>
     </section>
   )
